f4b924bcbb4dd131482515399f173399bd82d5d5
Sarvagya Surawat <sarvagyasurawat@gmail.com>
2023-04-26 16:29:46 +0530

    Delete FileExtensionNotSupportedException.java

--- src/main/java/org/example/exception/FileExtensionNotSupportedException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exception;
-
-public class FileExtensionNotSupportedException extends Exception {
-    public FileExtensionNotSupportedException(String fileFormatNotSupported) {
-        super(fileFormatNotSupported);
-    }
-}
